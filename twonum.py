b2d4dee135c1b1d8ffeac7d54aeb1ab9aa321f91
Danielle Benson <d.benson96@yahoo.com>
2020-04-01 16:49:12 -0500

    fixed grammer in num2 greater than num1 message

--- twonum.py
+++ twonum.py
@@ -5,6 +5,6 @@ if num1 > num2:
     print('Number 1 is bigger than Number 2')
 else:
     if num2 > num1:
-        print('num 2 is bigger than num 1')
+        print('Number 2 is bigger than Number 1')
     else:
         print('These numbers are equal')
