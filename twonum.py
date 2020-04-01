9cdf102f0030fa009af4493414e9592a33875172
Danielle Benson <d.benson96@yahoo.com>
2020-04-01 16:33:08 -0500

    checks if num1 or num2 is bigger

--- twonum.py
+++ twonum.py
@@ -4,5 +4,7 @@ num2 = input('enter number 2: ')
 if num1 > num2:
     print('num 1 is bigger than num2')
 else:
-    print('')
-    
+    if num2 > num1:
+        print('num 2 is bigger than num 1')
+    else:
+        print('')
