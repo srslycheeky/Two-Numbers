76ad30f8d6708b620f8c1ebfdd5c4b52e894a098
Danielle Benson <d.benson96@yahoo.com>
2020-04-01 16:39:29 -0500

    fixed grammar in compare message

--- twonum.py
+++ twonum.py
@@ -2,7 +2,7 @@ num1 = input('Enter number 1: ')
 num2 = input('enter number 2: ')
 
 if num1 > num2:
-    print('num 1 is bigger than num2')
+    print('Number 1 is bigger than Number 2')
 else:
     print('')
     
